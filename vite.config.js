1a98196e9b1b4c9c6d3109b87564d314101d796e
Your Name <you@example.com>
2024-10-23 14:01:34 +0200

    RESET

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext",
+  },
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "https://4hmm5a-ih.myshopify.com", 
+        target: "https://4hmm5a-ih.myshopify.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""), 
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
