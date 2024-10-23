e462c2f9f475081706983196e211c56195732806
Your Name <you@example.com>
2024-10-23 17:26:28 +0200

    proxy settings 0.10

--- src/data/getData.jsx
+++ src/data/getData.jsx
@@ -9,62 +9,59 @@ export const useProducts = () => {
     queryKey: ["products"],
     queryFn: async () => {
       try {
-        const response = await axios.get(
+        const response = await axios.post(
           "https://idyllic-concha-a54637.netlify.app/.netlify/functions/proxy/api/2024-10/graphql.json",
           {
-            headers: {
-              "X-Shopify-Storefront-Access-Token": storefrontAccessToken,
-            },
-            params: {
-              query: `
+            query: `
               {
-                    products(first: 10) {
-                      edges {
-                        node {
-                          id
-                          title
-                          priceRange {
-                            minVariantPrice {
+                products(first: 10) {
+                  edges {
+                    node {
+                      id
+                      title
+                      priceRange {
+                        minVariantPrice {
+                          amount
+                        }
+                      }
+                      featuredImage {
+                        url
+                        altText
+                      }
+                      variants(first: 10) {
+                        edges {
+                          node {
+                            id
+                            title
+                            priceV2 {
                               amount
+                              currencyCode
                             }
-                          }
-                          featuredImage {
-                            url
-                            altText
-                          }
-                          variants(first: 10) {
-                            edges {
-                              node {
-                                id
-                                title
-                                priceV2 {
-                                  amount
-                                  currencyCode
-                                }
-                                availableForSale
-                              }
-                            }
+                            availableForSale
                           }
                         }
                       }
                     }
                   }
-
-              `,
+                }
+              }
+            `,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              "X-Shopify-Storefront-Access-Token": storefrontAccessToken,
             },
           }
         );
-        // console.log(response.data);
         return response.data;
       } catch (error) {
         console.error("Error fetching products:", error);
         throw new Error("Failed to fetch products.");
       }
     },
-
     staleTime: 1000 * 60 * 5,
     cacheTime: 1000 * 60 * 10,
-
     onError: (error) => {
       console.log("Error fetching products: ", error);
     },
