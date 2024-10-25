c4c6146141f587036f67332d9762c1547c1f393e
Your Name <you@example.com>
2024-10-25 15:29:28 +0200

    domain updt_TROUBLE: back to alias

--- src/data/getData.jsx
+++ src/data/getData.jsx
@@ -10,7 +10,7 @@ export const useProducts = () => {
     queryFn: async () => {
       try {
         const response = await axios.post(
-          "https://scutigera.net/.netlify/functions/proxy/api/2024-10/graphql.json",
+          "https://idyllic-concha-a54637.netlify.app/.netlify/functions/proxy/api/2024-10/graphql.json",
           {
             query: `
               {
