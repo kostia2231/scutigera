af1d8adc717f8c5913f4ef83d2c0c3240272b747
Your Name <you@example.com>
2024-10-26 10:52:30 +0200

    cringe deleted

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -4,7 +4,7 @@ import useCartStore from "../../store/storeCart";
 export default function Navbar() {
   const count = useCartStore((state) => state.getCartCount());
   return (
-    <div className="flex gap-2 fixed w-[50%] justify-between pr-2 max-[640px]:w-[100%] max-[640px]:bg-white/95 max-[640px]:px-4 max-[640px]:py-4 z-30">
+    <div className="flex gap-2 fixed w-[50%] justify-between pr-2 max-[640px]:w-[100%] max-[640px]:bg-white max-[640px]:px-4 max-[640px]:py-4 z-30">
       <NavLink
         className="underline-offset-2 hover:underline active:opacity-70 max-[640px]:hover:no-underline"
         to="/"
