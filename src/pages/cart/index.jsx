23f6484bd8004ef1165ab09aca14042509e285bc
Your Name <you@example.com>
2024-10-28 20:39:34 +0100

    transition test 0.6

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -14,22 +14,20 @@ export default function Cart() {
   return (
     <>
       <div className="flex p-20 max-[640px]:p-0 max-[640px]:pt-20 max-[640px]:flex-col">
-        <div className="flex flex-col gap-1 w-[50%] max-[640px]:w-[100%] max-[640px]:order-3">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            delay: 0.5,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+          className="flex flex-col gap-1 w-[50%] max-[640px]:w-[100%] max-[640px]:order-3"
+        >
           {cart.map((product, index) => (
-            <motion.CartProduct
-              key={product.id}
-              product={product}
-              count={index + 1}
-              initial={{ opacity: 0, scale: 0.5 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{
-                duration: 0.8,
-                delay: 0.5,
-                ease: [0, 0.71, 0.2, 1.01],
-              }}
-            />
+            <CartProduct key={product.id} product={product} count={index + 1} />
           ))}
-        </div>
+        </motion.div>
         <div className="w-[50%] pl-1 flex flex-col gap-1 max-[640px]:w-[100%] max-[640px]:order-1 max-[640px]:p-0 max-[640px]:pb-10 max-[640px]:px-4 border-b h-fit pb-10 border-black-20 mb-10">
           <div className="flex justify-between">
             TOTAL <div className="font-bold">{getTotalPrice()}.0€</div>
