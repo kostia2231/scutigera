7f4df77f010ea9178e7ba60929cfc62ccf7670ba
Your Name <you@example.com>
2024-10-28 20:30:27 +0100

    transition test 0.4reset2

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -4,7 +4,7 @@ import useCartStore from "../../store/storeCart";
 export default function Navbar() {
   const count = useCartStore((state) => state.getCartCount());
   return (
-    <div className="flex gap-2 fixed w-[50%] justify-between pr-2 max-[640px]:w-[100%] max-[640px]:bg-white max-[640px]:px-4 max-[640px]:py-4 z-30 ml-1 mt-1">
+    <div className="flex gap-2 fixed w-[50%] justify-between pr-2 max-[640px]:w-[100%] max-[640px]:bg-white max-[640px]:px-4 max-[640px]:py-4 z-30 ml-1 mt-1 max-[640px]:m-0">
       <NavLink
         className="underline-offset-[3px] decoration-[1.5px] hover:underline active:opacity-70 max-[640px]:hover:no-underline"
         to="/"
