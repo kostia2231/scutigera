3dada090be075ed9c529c80766ccfd36c17e94d3
Your Name <you@example.com>
2024-10-28 21:10:16 +0100

    transition test 0.13

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -18,13 +18,14 @@ export default function Cart() {
           {cart.map((product, index) => (
             <>
               <motion.div
+                style={{ willChange: "opacity, transform" }}
                 key={product.id}
                 initial={{ opacity: 0, y: 30 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{
                   duration: 0.6,
                   delay: index * 0.2,
-                  ease: [0, 0.71, 0.2, 1.01],
+                  ease: "easeInOut",
                 }}
               >
                 <CartProduct product={product} count={index + 1} />
--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -8,11 +8,12 @@ export default function Main() {
 
   return (
     <motion.div
+      style={{ willChange: "opacity, transform" }}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{
         duration: 0.6,
-        ease: [0, 0.71, 0.2, 1.01],
+        ease: "easeInOut",
       }}
       className="flex flex-col max-[640px]:mx-4 max-[640px]:pt-[31px] pb-10"
     >
