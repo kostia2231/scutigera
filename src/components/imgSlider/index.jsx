a8515327c36498fe75778d914a12b5db89f4c1ab
Your Name <you@example.com>
2024-10-28 15:05:44 +0100

    slider/img size mobile

--- src/components/imgSlider/index.jsx
+++ src/components/imgSlider/index.jsx
@@ -12,7 +12,7 @@ export function ImgSlider({ imgUrls }) {
     <>
       <div className="mb-1 max-[640px]:mb-0">
         <img
-          className="cursor-pointer"
+          className="object-cover cursor-pointer h-[60vh]"
           onClick={showNextImg}
           src={imgUrls[imageIndex].url}
         />
