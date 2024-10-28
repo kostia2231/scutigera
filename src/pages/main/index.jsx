14a192a04843ede05cb2faeae91bcac5f5a44baa
Your Name <you@example.com>
2024-10-28 20:16:56 +0100

    transition test 0.2

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,18 +1,24 @@
 import Product from "../../components/product";
 import { useProducts } from "../../data/getData";
+import { useEffect, useState } from "react";
 
 export default function Main() {
   const { data } = useProducts();
   const products = data?.data.products.edges;
+  const [renderedProducts, setRenderedProducts] = useState([]);
+
+  useEffect(() => {
+    if (products) {
+      document.startViewTransition?.(() => {
+        setRenderedProducts(products);
+      });
+    }
+  }, [products]);
 
   return (
     <div className="flex flex-col max-[640px]:mx-4 max-[640px]:pt-[31px] pb-10">
-      {products?.map((item) => (
-        <Product
-          key={item.node.id}
-          item={item.node}
-          className="product-appear"
-        />
+      {renderedProducts?.map((item) => (
+        <Product key={item.node.id} item={item.node} />
       ))}
     </div>
   );
