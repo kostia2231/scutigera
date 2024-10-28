39e38bdeba8a3c744d8e7caaea9f5430cd559782
Your Name <you@example.com>
2024-10-28 20:07:08 +0100

    transition test 0.1

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -5,15 +5,11 @@ export default function Main() {
   const { data } = useProducts();
   const products = data?.data.products.edges;
 
-  // console.log("Main", products);
-  // console.log(products);
   return (
     <div className="flex flex-col max-[640px]:mx-4 max-[640px]:pt-[31px] pb-10">
-      {products?.map((item) => (
-        <Product key={item.node.id} item={item.node} />
-      ))}
+      {document.startViewTransition(() =>
+        products?.map((item) => <Product key={item.node.id} item={item.node} />)
+      )}
     </div>
   );
 }
-
-// max-[640px]:bg-black
