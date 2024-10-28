445a1ee9812c6a0d40276b604ddd5435b89c9f3b
Your Name <you@example.com>
2024-10-28 18:54:49 +0100

    UI: updt

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -18,7 +18,7 @@ export default function Cart() {
             <CartProduct key={product.id} product={product} count={index + 1} />
           ))}
         </div>
-        <div className="w-[50%] pl-1 flex flex-col gap-1 max-[640px]:w-[100%] max-[640px]:order-1 max-[640px]:p-0 max-[640px]:pb-20 max-[640px]:px-4">
+        <div className="w-[50%] pl-1 flex flex-col gap-1 max-[640px]:w-[100%] max-[640px]:order-1 max-[640px]:p-0 max-[640px]:pb-10 max-[640px]:px-4 border-b h-fit pb-10 border-black-20 mb-10">
           <div className="flex justify-between">
             TOTAL <div className="font-bold">{getTotalPrice()}.0€</div>
           </div>
@@ -31,7 +31,6 @@ export default function Cart() {
             <AddToCartButton items={cartData} />
           )}
         </div>
-        <div className="border-b border-black-20 max-[640px]:order-2 mb-5 mx-4 hidden max-[640px]:block"></div>
       </div>
     </>
   );
