0f2b698e7e2101f70ea549d818c5c1d1f33cb00a
Your Name <you@example.com>
2024-10-29 23:20:29 +0100

    click on logo

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -5,12 +5,17 @@ import useSliderStore from "../../store/imgSliderStore";
 export default function Navbar() {
   const { resetSliders } = useSliderStore();
   const count = useCartStore((state) => state.getCartCount());
+  const onLogoClick = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+    resetSliders();
+  };
+
   return (
     <div className="flex gap-2 fixed w-[50%] justify-between pr-2 max-[640px]:w-[100%] max-[640px]:bg-white max-[640px]:px-4 max-[640px]:py-4 z-30 ml-1 mt-1 max-[640px]:m-0">
       <NavLink
         className="underline-offset-[3px] decoration-[1.5px] hover:underline active:opacity-70 max-[640px]:hover:no-underline"
         to="/"
-        onClick={resetSliders}
+        onClick={onLogoClick}
       >
         SCUTIGERA
       </NavLink>
