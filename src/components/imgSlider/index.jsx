c0d907af2fda5278ccedfa51ba2ffcceb11c774a
Your Name <you@example.com>
2024-10-31 22:50:22 +0100

    gallery fixws

--- src/components/imgSlider/index.jsx
+++ src/components/imgSlider/index.jsx
@@ -1,13 +1,13 @@
 import PropTypes from "prop-types";
 import useSliderStore from "../../store/imgSliderStore";
 import { motion } from "framer-motion";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
 export default function ImgSlider({ imgUrls, id }) {
   const { sliders, setActiveSlider, setImageIndex, resetSliders } =
     useSliderStore();
   const imageIndex = sliders[id] || 0;
-  const [prevIndex, setPrevIndex] = useState(imageIndex);
+  // const [prevIndex, setPrevIndex] = useState(imageIndex);
 
   useEffect(() => {
     const nextIndex = (imageIndex + 1) % imgUrls.length;
@@ -16,7 +16,7 @@ export default function ImgSlider({ imgUrls, id }) {
   }, [imageIndex, imgUrls]);
 
   const showNextImg = () => {
-    setPrevIndex(imageIndex);
+    // setPrevIndex(imageIndex);
     setImageIndex(id, (imageIndex + 1) % imgUrls.length);
   };
 
@@ -28,27 +28,12 @@ export default function ImgSlider({ imgUrls, id }) {
 
   return (
     <div className="relative h-[100vh] w-full max-[640px]:h-[60vh]">
-      {/* Предыдущее изображение */}
       <motion.img
-        key={`prev-${prevIndex}`}
-        initial={{ opacity: 1 }}
-        animate={{ opacity: 0.8 }}
-        transition={{ duration: 0.2 }}
-        src={imgUrls[prevIndex].url}
-        className="absolute top-0 left-0 object-cover w-full h-full will-change-transform will-change-opacity will-change-filter"
-        alt="Previous Image"
-      />
-
-      {/* Текущее изображение */}
-      <motion.img
-        key={`current-${imageIndex}`}
-        initial={{ opacity: 0.8 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.2 }}
+        key={imageIndex}
         src={imgUrls[imageIndex].url}
         className="absolute top-0 left-0 object-cover w-full h-full cursor-pointer will-change-transform will-change-opacity will-change-filter"
         onClick={onClick}
-        alt="Current Image"
+        alt="Image"
       />
     </div>
   );
