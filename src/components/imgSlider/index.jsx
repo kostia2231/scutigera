9b5e90c4e04094e77cc8e3b5d8384659796435fd
Your Name <you@example.com>
2024-10-31 22:56:24 +0100

    gallery fixes

--- src/components/imgSlider/index.jsx
+++ src/components/imgSlider/index.jsx
@@ -1,39 +1,41 @@
 import PropTypes from "prop-types";
 import useSliderStore from "../../store/imgSliderStore";
-import { motion } from "framer-motion";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function ImgSlider({ imgUrls, id }) {
   const { sliders, setActiveSlider, setImageIndex, resetSliders } =
     useSliderStore();
   const imageIndex = sliders[id] || 0;
-  // const [prevIndex, setPrevIndex] = useState(imageIndex);
+  const [isNextImageLoaded, setIsNextImageLoaded] = useState(true);
 
   useEffect(() => {
     const nextIndex = (imageIndex + 1) % imgUrls.length;
     const nextImage = new Image();
     nextImage.src = imgUrls[nextIndex].url;
+    nextImage.onload = () => setIsNextImageLoaded(true);
   }, [imageIndex, imgUrls]);
 
   const showNextImg = () => {
-    // setPrevIndex(imageIndex);
+    setIsNextImageLoaded(false);
     setImageIndex(id, (imageIndex + 1) % imgUrls.length);
   };
 
   const onClick = () => {
     resetSliders(id);
     setActiveSlider(id);
-    showNextImg();
+    if (isNextImageLoaded) {
+      showNextImg();
+    }
   };
 
   return (
     <div className="relative h-[100vh] w-full max-[640px]:h-[60vh]">
-      <motion.img
+      <img
         key={imageIndex}
         src={imgUrls[imageIndex].url}
-        className="absolute top-0 left-0 object-cover w-full h-full cursor-pointer will-change-transform will-change-opacity will-change-filter"
+        className="absolute top-0 left-0 object-cover w-full h-full cursor-pointer"
         onClick={onClick}
-        alt="Image"
+        alt="Product Image"
       />
     </div>
   );
