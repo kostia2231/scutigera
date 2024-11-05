12422cea9e77c274906500f55d8f5d44123837a0
Your Name <you@example.com>
2024-11-05 16:15:53 +0100

    responsive fix

--- src/components/cartProduct/index.jsx
+++ src/components/cartProduct/index.jsx
@@ -26,8 +26,8 @@ export default function CartProduct({ product, count }) {
   };
 
   return (
-    <div className="flex gap-1 max-[640px]:mx-4 max-[640px]:justify-between">
-      <div>{count}.</div>
+    <div className="flex gap-1 max-[640px]:mx-4 max-[640px]:justify-between pr-2 max-[640px]:pr-0">
+      <div>{count}</div>
       <div
         className="bg-cover bg-center w-[250px] h-[300px] opacity-100 max-[640px]:h-[250px] max-[640px]:w-[180px]"
         style={{ backgroundImage: `url("${product.img}")` }}
