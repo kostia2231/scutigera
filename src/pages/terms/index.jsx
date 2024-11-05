25b7e174f8de27cf08570992e44ca851cd2e4769
Your Name <you@example.com>
2024-11-05 15:01:09 +0100

    terms link added

--- src/pages/terms/index.jsx
+++ src/pages/terms/index.jsx
@@ -1,7 +1,8 @@
 export default function Terms() {
   return (
-    <div className="flex p-20 max-[640px]:p-0 max-[640px]:pt-20 max-[640px]:flex-col min-[1275px]:px-[100px]">
-      <div className="w-[50%]">
+    <div className="grid grid-cols-2 p-20 max-[640px]:p-0 max-[640px]:pt-20 max-[640px]:flex-col min-[1275px]:px-[100px]">
+      <div></div>
+      <div className="pl-2">
         <div>TERMS</div>
       </div>
     </div>
