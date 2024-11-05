1790541c157ac43b79c84e2dcea81fd5f76ae7db
Your Name <you@example.com>
2024-11-05 14:03:28 +0100

    terms link added

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import S from "../../assets/svg/Scutigera.svg";
+import Terms from "../../pages/terms";
 
 export default function Footer() {
   return (
@@ -10,7 +11,9 @@ export default function Footer() {
         </div>
         <div className="flex gap-5 max-[640px]:gap-5">
           <div>ABOUT</div>
-          <div>TERMS</div>
+          <Link to="/terms">
+            <div>TERMS</div>
+          </Link>
           <Link to="https://www.instagram.com/scutigeraa/">
             <div className="pointer underline-offset-[3px] decoration-[1.5px] hover:underline active:opacity-70 max-[640px]:hover:no-underline">
               INSTAGRAM
---
+++ src/pages/terms/index.js
@@ -0,0 +1,75 @@
+export default function Terms() {
+  return (
+    <>
+      <div className="grid grid-cols-2">
+        <div>
+          <div>TERMS</div>
+          <div>
+            This Privacy Policy describes how SCUTIGERA (the "Site", "we", "us",
+            or "our") collects, uses, and discloses your personal information
+            when you visit, use our services, or make a purchase from
+            www.scutigera.online (the "Site") or otherwise communicate with us
+            regarding the Site (collectively, the "Services"). For purposes of
+            this Privacy Policy, "you" and "your" means you as the user of the
+            Services, whether you are a customer, website visitor, or another
+            individual whose information we have collected pursuant to this
+            Privacy Policy. Please read this Privacy Policy carefully.
+          </div>
+          <br />
+          <div>HOW WE COLLECT AND USE YOUR PERSONAL DATA</div>
+          <div>
+            To provide the Services, we collect personal information about you
+            from a variety of sources, as set out below. The information that we
+            collect and use varies depending on how you interact with us. In
+            addition to the specific uses set out below, we may use information
+            we collect about you to communicate with you, provide or improve or
+            improve the Services, comply with any applicable legal obligations,
+            enforce any applicable terms of service, and to protect or defend
+            the Services, our rights, and the rights of our users or others.
+          </div>
+          <br />
+          <div>WHAT PERSONAL DATA WE COLLECT</div>
+          <div>
+            The types of personal information we obtain about you depends on how
+            you interact with our Site and use our Services. When we use the
+            term "personal information", we are referring to information that
+            identifies, relates to, describes or can be associated with you. The
+            following sections describe the categories and specific types of
+            personal information we collect.
+          </div>
+          <br />
+          <div>DATA WE COLLECT DIRECTLY FROM YOU</div>
+          <div>
+            <ul>
+              <li>
+                Contact details including your name, address, phone number, and
+                email.
+              </li>
+              <li>
+                Order information including your name, billing address, shipping
+                address, payment confirmation, email address, and phone number.
+              </li>
+              <li>
+                Account information including your username, password, security
+                questions and other information used for account security
+                purposes.
+              </li>
+              <li>
+                Customer support information including the information you
+                choose to include in communications with us, for example, when
+                sending a message through the Services.
+              </li>
+            </ul>
+            <div>
+              Some features of the Services may require you to directly provide
+              us with certain information about yourself. You may elect not to
+              provide this information, but doing so may prevent you from using
+              or accessing these features.
+            </div>
+            <br />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
