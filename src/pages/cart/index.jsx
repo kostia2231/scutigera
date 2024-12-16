b8c2e883fb51d684def22d5520f84f77c6f2bfde
konstantin <kostia505@gmail.com>
2024-12-16 20:56:19 +0100

    checkout: fixed

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -93,11 +93,11 @@ export default function Cart() {
                   <br className="max-[870px]:hidden" />
                   Checkout is coming soon. Stay tuned.
                 </div>
-                <div className="text-end">
+                <div className="text-center bg-black/80 w-full text-white">
                   <a
                     href={`mailto:info@scutigera.online?subject=ORDER <3&body=HEY! I'D LIKE TO ORDER:
                     \n ${encodeURIComponent(withMailCartData)} \n HIT ME UP!`}
-                    className="p-1 border border-black/20 border-dashed font-bold w-fit underline-offset-[3px] decoration-[1.5px] hover:underline active:opacity-70 max-[640px]:hover:no-underline"
+                    className="p-1 font-bold w-fit underline-offset-[3px] decoration-[1.5px] hover:underline active:opacity-70 max-[640px]:hover:no-underline"
                   >
                     ORDER NOW
                   </a>
