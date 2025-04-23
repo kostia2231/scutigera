b65538cd30d3e193c7aaf4d3678318a95d72b9a1
konstantin <kostia505@gmail.com>
2025-04-23 14:59:08 +0200

    edited

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="opacity-40">
+    <footer className="opacity-40 pt-8">
       <div className="relative z-10">
         {/* <div>
           <img className="w-[250px] max-[640px]:w-[200px]" src={S} alt="" />
         </div> */}
-        <div className="flex gap-x-4 gap-y-2 w-[100%] flex-wrap justify-center px-4 pb-4 ">
+        <div className="flex gap-x-4 gap-y-2 w-[100%] flex-wrap justify-center px-4 pb-4">
           <Link to="/about">
             <div className="pointer underline-offset-[3px] decoration-[1.5px] hover:underline active:opacity-70 max-[640px]:hover:no-underline">
               ABOUT
