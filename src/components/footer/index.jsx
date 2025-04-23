8719465f1d3e3904a62a66185688e327a05b68ed
konstantin <kostia505@gmail.com>
2025-04-23 17:17:31 +0200

    edited

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
         {/* <div>
           <img className="w-[250px] max-[640px]:w-[200px]" src={S} alt="" />
         </div> */}
-        <div className="flex gap-x-4 gap-y-2 w-[100%] flex-wrap justify-center px-4 pb-4">
+        <div className="flex gap-x-4 gap-y-2 w-[100%] flex-wrap justify-center max-[640px]:justify-between px-4 pb-4">
           <Link to="/about">
             <div className="pointer underline-offset-[3px] decoration-[1.5px] hover:underline active:opacity-70 max-[640px]:hover:no-underline">
               ABOUT
